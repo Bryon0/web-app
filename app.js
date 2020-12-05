86e06f815cceb8b6821a071f68df6b851d99265a
unknown <bryonczaja@lewisu.edu>
2020-12-04 23:50:59 -0600

    Added database connection. Everything still in process

--- app.js
+++ app.js
@@ -7,46 +7,56 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mariadb = require('mariadb');
+const bcrypt = require('bcryptjs');
 
 //Create the express app object.
 const app = express();
 //Set the port being used from either the system variable or use
 // the default of 3000
 const PORT = process.env.PORT || 3000;
+const SALT = 10;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 
-//Set up the maria db
+//Set up the maria db pool
 const pool = mariadb.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
     password:  process.env.DB_PASSWORD,
     connectionLimit: 5});
+
+const dbconn = {
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password:  process.env.DB_PASSWORD,
+    connectionLimit: 5
+};
  
-async function asyncFunction() {
-    let conn;
-    try {
-        console.log('Attempting to connect to the database.');
-        conn = await pool.getConnection();
-        console.log('Connected to the database.');
-   
-    } catch (err) {
-      console.log(err);
-    } finally {
-      if (conn) {
-          conn.release(); //release to pool
-      }
-      console.log('Disconnected from the database.');
-    }
-}
-
-asyncFunction().catch( e => {
-    console.log(e);
-})
-
 app.get('/register', (req, res) => {
+    //TODO: Check if the  user exists.  If not create user in the databse.
+    const email = req.body.email;
+    const fname = req.body.firstname;
+    const lname = req.body.lastname;
+    const password = req.body.password;
+
+    mariadb.createConnection(dbconn)
+    .then(conn => {
+      conn.query("SELECT * FROM user WHERE Email=?", [email])
+        .then(rows => {
+          console.log(rows); 
+          conn.end();
+        })
+        .catch(err => { 
+          //handle query error
+        });
+    })
+    .catch(err => {
+      //handle connection error
+    });
+    //Encrypt the password.
+    //const hash = bcrypt.hash(req.body.password, SALT);
     res.render('register');
 });
 
@@ -56,7 +66,22 @@ app.post('/register', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login');
-})
+});
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let hashpassword = "";
+
+    //const isMatch = await bcrypt.compare(req.body.password, password);
+
+    //Is it necessaray to use async to log in?s
+    try {
+        let conn = pool.getConnection();
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 app.use(function (req, res, next) {
     res.render('404')
